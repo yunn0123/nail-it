87cfc1811f316f19623d05bdc3ac7e8b9f4a9cb8
yunn0123 <fefe20030123@gmail.com>
2025-05-27 18:14:12 +0800

    add docker-entrypoint.sh

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# docker-entrypoint.sh
+# 用於啟動 Nginx 前，將環境變數注入 nginx.conf
+
+# 將 BACKEND_API_URL 預設為 http://localhost:3000（如未設置）
+BACKEND_API_URL=${BACKEND_API_URL:-http://localhost:3000}
+
+# 用 envsubst 將模板中的 ${BACKEND_API_URL} 替換為實際值
+envsubst '${BACKEND_API_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# 啟動 Nginx
+exec "$@"
